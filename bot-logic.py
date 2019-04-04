5ea36faf7209674e3b42907e7c8536f3a6dd7e2e
ZHAO Xudong <zxdong@gmail.com>
2019-04-04 10:10:40 +0800

    Support auto translate (#1)

--- bot-logic.py
+++ bot-logic.py
@@ -40,6 +40,11 @@ langCodes = {
   'tr': 'Turkish'
 }
 
+botName = 'translate-bot'
+
+def defaultLang():
+  return 'English[en]'
+
 def createLangList():
   res = ''
   for k in langKeys:
@@ -49,28 +54,33 @@ def createLangList():
 langKeys = langCodes.keys()
 langList = createLangList()
 
-def helpMsg(botId):
-  return f'''Hello, I am a translate chatbot. Please reply "@![:Person]({botId}) **[sourceLangCode]>[targetLangCode] [xxxxxx]**" if you want to translate something.
+def helpMsg(botId, defaultLang):
+  return f'''Hello, I am a translate chatbot. Please reply "![:Person]({botId}) **[sourceLangCode]>[targetLangCode] [xxxxxx]**" if you want to translate something.
 
 **Example**
 
-@![:Person]({botId}) **en>fr Hello world**
+![:Person]({botId}) **en>fr Hello world**
 
 Or use auto detect
 
-@![:Person]({botId}) **>fr Hello world**
+![:Person]({botId}) **>fr Hello world**
 
-You can read about supported language code from [aws translate document](https://docs.aws.amazon.com/translate/latest/dg/API_TranslateText.html) or use "**@![:Person]({botId}) langs**"
+Or auto translate to **default language: {defaultLang}**
+
+![:Person]({botId}) Hello world**
+
+You can also set default language by post **![:Person]({botId}) set [langCode]**
+
+You can read about supported language code from [aws translate document](https://docs.aws.amazon.com/translate/latest/dg/API_TranslateText.html) or post "**![:Person]({botId}) langs**"
 
   '''
-
 
 def botJoinPrivateChatAction(bot, groupId, user, dbAction):
   """
   bot join private chat event handler
   bot could send some welcome message or help, or something else
   """
-  text = helpMsg(bot.id)
+  text = helpMsg(bot.id, defaultLang())
   bot.sendMessage(
     groupId,
     {
@@ -95,41 +105,86 @@ def botGotPostAddAction(
   if not f'![:Person]({bot.id})' in text or handledByExtension:
     return
 
-  msg = helpMsg(bot.id)
   langErr = f'''You can only choose language code from this list:
 
 {langList}
   '''
-  m = re.match(r'.+ +langs', text)
-  if not m is None:
-    return bot.sendMessage(
-      groupId,
-      {
-        'text': langErr
-      }
-    )
-
-  m = re.match(r'.+ ([\w-]+)?>([\w-]+) (.+)', text)
-  if m is None:
+  def sendMsg(msg):
     bot.sendMessage(
       groupId,
       {
         'text': msg
       }
     )
-    return
+  where = {
+    'id': f'{bot.id}_{groupId}_{botName}'
+  }
+  inst = dbAction('user', 'get', where)
+  defaultLangStr = defaultLang()
+  defaultLangCode = 'en'
+  if not inst == False:
+    defaultLangStr = inst['data']['defaultLangStr']
+    defaultLangCode = inst['data']['defaultLangCode']
+  msg = helpMsg(bot.id, defaultLangStr)
+  print('defaultLangStr', defaultLangStr)
+  print('defaultLangCode', defaultLangCode)
 
-  src = m.group(1)
-  tar = m.group(2)
-  txt = m.group(3)
+  # no content
+  m1 = re.match(r'[^ ]+ *$', text)
+  if not m1 is None:
+    return sendMsg(msg)
+
+  # set default language
+  m2 = re.match(r'[^ ]+ +set +(.+)', text)
+  if not m2 is None:
+    tar = m2.group(1)
+    if not tar in langCodes:
+      return sendMsg(f'''{tar} not in supported language list.
+{langErr}
+''')
+    langName = langCodes[tar]
+    defaultLangStr0 = f'{langName}[{tar}]'
+    if not inst == False:
+      dbAction('user', 'update', {
+        'id': where['id'],
+        'update': {
+          'data': {
+            'defaultLangStr': defaultLangStr0,
+            'defaultLangCode': tar
+          }
+        }
+      })
+    else:
+      dbAction('user', 'add', {
+        'id': where['id'],
+        'data': {
+          'defaultLangStr': defaultLangStr0,
+          'defaultLangCode': tar
+        }
+      })
+    return sendMsg(f'Default language set to [{tar}]')
+
+  # list langs
+  m = re.match(r'[^ ]+ +langs', text)
+  if not m is None:
+    return sendMsg(langErr)
+
+  # translate with target language
+  m = re.match(r'[^ ]+ ([\w-]+)?>([\w-]+) (.+)', text)
+  src = None
+  tar = ''
+  txt = ''
+  if m is None:
+    m3 = re.match(r'[^ ]+ +(.+)', text)
+    txt = m3.group(1)
+    tar = defaultLangCode
+  else:
+    src = m.group(1)
+    tar = m.group(2)
+    txt = m.group(3)
 
   if (not src is None and not src in langKeys) or not tar in langKeys:
-    bot.sendMessage(
-      groupId,
-      {
-        'text': langErr
-      }
-    )
+    sendMsg(langErr)
     return
 
   translate = boto3.client(
@@ -146,13 +201,10 @@ def botGotPostAddAction(
   transCodeSrc = result.get('SourceLanguageCode') or 'unknown'
   transCodeTar = result.get('TargetLanguageCode') or 'unknown'
 
-  final = f'''![:Person]({creatorId}) Translate result:
+  final = f'''![:Person]({creatorId}) says:
+**{trans}**
 
-  **{transCodeSrc}: {langCodes[transCodeSrc]}** > **{transCodeTar}: {langCodes[transCodeTar]}**
-
-  **{trans}**
-
-
+**{transCodeSrc}: {langCodes[transCodeSrc]}** > **{transCodeTar}: {langCodes[transCodeTar]}**
 > {txt}
   '''
 
@@ -160,10 +212,5 @@ def botGotPostAddAction(
   print('SourceLanguageCode: ' + transCodeSrc)
   print('TargetLanguageCode: ' + transCodeTar)
 
-  return bot.sendMessage(
-    groupId,
-    {
-      'text': final
-    }
-  )
+  sendMsg(final)
 
